e3f3f46d4ff2fadb818c5883ad8c9d691401b405
amar <66710686+amarouldhamadouche@users.noreply.github.com>
2022-11-28 00:26:46 +0100

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,42 +1,38 @@
 import styles from '../styles/Contact.module.css'
+import emailjs from "@emailjs/browser"
 import axios from 'axios'
 import { useState } from 'react'
 export default function Contact ()  {
  const [isSent,setIsSent] = useState(false)
  const [isError,setIsError] = useState(false)
- const onSubmitHandler = async(e)=>{
-  e.preventDefault()
-  const formData = {}
-  Array.from(e.currentTarget.elements).forEach(feild=>{
-   if(!feild.name)
-    return
-   formData[feild.name] = feild.value
-  })
-  try{
-   typeof(window)!=="undefined" &&
-   await axios({
-   method: 'POST',
-   url :`${window.location.origin}/api/mail`, 
-   contentType:'application/json',
-   data : formData})
-   setIsError(false)
-   setIsSent(true)
-   }catch(err){
+ const form = useRef();
+ const sendEmail = (e) => {
+    e.preventDefault();
     setIsSent(false)
-    setIsError(true)
- }
+    setIsError(false)
+    emailjs.sendForm(
+        "service_z5i1b2g",
+        "template_9ogtkl1",
+        
+        form.current,
+        "K6ilKK1ZpxgMgxf9W"
+      )
+      .then(
+        (result) => {
+          setIsSent(true)
+        },
+        (error) => {
+          setIsError(true)
+        }
+      );
+  }; 
 
- }
   return (
    <div className={styles.container}>
    <h1 className={styles.title}>Send us an email </h1>
-   <form className={styles.form} onSubmit={onSubmitHandler}>
-    <label className={styles.label} htmlFor='name'>name :</label>
-    <input className={styles.input} required='true' id='name' name='name' type='text'/>
-    <label className={styles.label} htmlFor='email'>email :</label>
-    <input className={styles.input}  required='true' id='email' name='email' type='email'/>
+   <form ref={form} className={styles.form} onSubmit={sendEmail}>email'/>
     <label className={styles.label} htmlFor='message'>message :</label>
-    <textarea rows={5} className={styles.textArea} required='true' id='message' name='message' type='text'/>
+    <textarea placholder="type your message here, and don't forget to send your email" rows={5} className={styles.textArea} required='true' id='message' name='message' type='text'/>
     <button className={styles.button } type='submit'>Send</button>
    </form>
    {isSent && <div style={{color:'teal',marginTop:'20px'}}>the mail was sent successffuly!</div>}
